0b1171a6e9c697c314146dcc1b7782bdba93b186
Matthias Streulens <matthias.streulens@geosparc.com>
2014-01-24 10:46:10 +0100

    MAJ-1096: update copyright year

--- src/main/java/org/mypackage/client/ApplicationLayout.java
+++ src/main/java/org/mypackage/client/ApplicationLayout.java
@@ -1,7 +1,7 @@
 /*
  * This is part of Geomajas, a GIS framework, http://www.geomajas.org/.
  *
- * Copyright 2008-2013 Geosparc nv, http://www.geosparc.com/, Belgium.
+ * Copyright 2008-2014 Geosparc nv, http://www.geosparc.com/, Belgium.
  *
  * The program is available in open source according to the GNU Affero
  * General Public License. All contributions in this program are covered
